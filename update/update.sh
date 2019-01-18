117c39322d0eb3647380305b4275097a5750270a
Alec Snyder <linuxbash8@gmail.com>
2019-01-18 11:07:29 -0800

    change update script

--- update/update.sh
+++ update/update.sh
@@ -4,9 +4,7 @@ set -e
 sudo pacman -Syu
 echo -e "\n"
 echo -e "\e[34mThese packages are in need of and update:\n"
-cower -u
+auracle sync
 echo -e "\e[0m"
-sudo pacman -Scc
-echo -e "\n"
 echo -e "\e[31mOrphan packages: \e[0m"
 sudo pacman -Qdt
