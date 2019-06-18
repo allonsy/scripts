7e95c8eaa826244a6a2f35b49fdaf7d16146a8e8
Alec Snyder <linuxbash8@gmail.com>
2019-06-17 23:55:30 -0700

    fix update script colors

--- update/update.sh
+++ update/update.sh
@@ -4,7 +4,8 @@ set -e
 sudo pacman -Syu
 echo -e "\n"
 echo -e "\e[34mThese packages are in need of and update:\n"
-auracle sync
 echo -e "\e[0m"
+auracle sync
+echo -e "\n"
 echo -e "\e[31mOrphan packages: \e[0m"
 sudo pacman -Qdt
