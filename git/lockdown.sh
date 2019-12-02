f7673ad3a84be619a0ee11a5041ac1eb52f3c77f
Alec Snyder <linuxbash8@gmail.com>
2019-12-02 13:02:53 -0800

    add fsck to lockdown script

--- git/lockdown.sh
+++ git/lockdown.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # locks down a git repository to only accept signed commits
+set -x
 
-KEYUID="72FC4E7F29E6D8491228DA55E6EAD3EBE3CC6E71"
+KEYUID="1D709E45FA9739EB9EC620240CA1D8E66C99F04F"
 
 git config user.signingkey $KEYUID
 git config commit.gpgSign true
 git config merge.verifySignatures true
+git config receive.fsckObjects true
