97db7c3c0038ad53433aa7f82c8459504384349a
Alec Snyder <linuxbash8@gmail.com>
2020-02-05 17:43:48 -0800

    add basic package parsing

--- update/update.sh
+++ update/update.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 set -e
-sudo pacman -Syu
-echo -e "\n"
-echo -e "\e[34mThese packages are in need of and update:\n"
-echo -e "\e[0m"
-auracle sync
-echo -e "\n"
-echo -e "\e[31mOrphan packages: \e[0m"
-sudo pacman -Qdt
+
+# sudo pacman -Sy
+readarray PACKAGE_LIST <<< $(sudo pacman -Su --print-format "%n %v")
+for PACKAGE in "${PACKAGE_LIST[@]}"; do
+    read -r -a PACKAGE_VALS <<< "$PACKAGE"
+    echo "Name: ${PACKAGE_VALS[0]}"
+    echo "Version: ${PACKAGE_VALS[1]}"
+done
+# echo -e "\n"
+# echo -e "\e[34mThese packages are in need of and update:\n"
+# echo -e "\e[0m"
+# auracle sync
+# echo -e "\n"
+# echo -e "\e[31mOrphan packages: \e[0m"
+# sudo pacman -Qdt
